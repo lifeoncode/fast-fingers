032ae2f1b7ed23dac97eaa42fa1a51ca11d0cb21
lifeoncode <nduduzo@yellogarden.co.za>
2022-10-04 14:10:26 +0200

    figuring it out

--- js/index.js
+++ js/index.js
@@ -9,36 +9,52 @@ window.onload = () => {
     const ui = new UI();
     ui.displayWords();
 
-    // listen for keypress
-    window.addEventListener('keypress', matchKeys);
+    window.addEventListener('keypress', gamePlay);
+}
+
+
+// all the logic of the game
+const gamePlay = (e) => {
+    const ui = new UI();
+    const currentWords = ui.container.textContent; 
+    let currentLetter = currentWords[count.getCount()];
+
+    if (matchKeys(e.key)) {
+        console.log('GREAT!');
+        if (e.key === ' ') {
+            document.querySelector('.space').classList.add('correct');
+        } else {
+            document.querySelector(`.${currentLetter}`).classList.add('correct');
+        }
+
+    } else {
+        console.log('OH NO...');
+        if (e.key === ' ') {
+            document.querySelector('.space').classList.add('wrong');
+        } else {
+            document.querySelector(`.${currentLetter}`).classList.add('wrong');
+        }
+    }
 }
 
 
 // match keys entered with words on page
 let count = new Count();
-const matchKeys = (e) =>  {
+const matchKeys = (pressedKey) =>  {
     // instatiate needed classes
     const ui = new UI();
 
     // words - current letter and entered key
     const currentWords = ui.container.textContent; 
-
-    // individual letters for styling
-    const letters = currentWords.split('');
-    for (let i of letters) {
-        ui.createElement('span', i);
-    }
-    
     let currentLetter = currentWords[count.getCount()];
-    let enteredLetter = e.key;
     // increment the count by 1 with every keypress
     count.increment();
 
     // the matching logic
-    if (enteredLetter === currentLetter) {
-        console.log(`matched ${currentLetter}`);
+    if (pressedKey === currentLetter) {
+        return true;
     } else {
-        console.log('wrong...');
+        return false;
     }
 }
 
