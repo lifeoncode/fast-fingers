6df56a28ac5a1cc6ea45e00c3056e0bc232a6bad
lifeoncode <nduduzo@yellogarden.co.za>
2022-10-31 15:27:08 +0200

    accounting for right and wrong answers

---
+++ js/app.js
@@ -0,0 +1,119 @@
+// UI logic
+export class UI {
+  constructor() {
+    this.textContainer = document.querySelector("#text");
+    this.scoreContainer = document.querySelector("#score");
+    this.sound = document.querySelector("audio");
+    this.stats = new Statistics();
+  }
+
+  playKeySound = () => {
+    this.sound.play();
+  };
+
+  generateText = async () => {
+    const res = await fetch("https://api.chucknorris.io/jokes/random");
+    const resData = await res.json();
+    let joke = resData.value.split("");
+
+    text.innerHTML = "";
+    for (let char of joke) {
+      text.innerHTML += `<span>${char}</span>`;
+    }
+  };
+
+  correctLetter = (letter) => {
+    letter.className = "correct";
+    this.updateScore();
+  };
+
+  wrongLetter = (letter) => {
+    letter.className = "wrong";
+  };
+
+  updateScore = () => {
+    try {
+      let stats = this.stats.getStats();
+      this.scoreContainer.innerHTML = `
+      <span>correct: ${stats.score[0]}</span>
+      <span>incorrect: ${stats.score[1]}</span>`;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
+
+// game logic
+export class Gameplay {
+  constructor() {
+    this.ui = new UI();
+    this.stats = new Statistics();
+  }
+
+  handleInputLogic = (enteredKeys, text) => {
+    text.forEach((char, index) => {
+      let charEntered = enteredKeys[index];
+
+      if (charEntered == null) {
+        char.className = "";
+      } else if (charEntered === char.innerText) {
+        this.ui.correctLetter(char);
+      } else {
+        this.ui.wrongLetter(char);
+      }
+    });
+
+    // when the end of a sentence is reached
+    if (enteredKeys.length === text.length) {
+      for (let span of text) {
+        if (span.className === "correct") {
+          this.stats.incrementScore("correct");
+        } else if (span.className === "wrong") {
+          this.stats.incrementScore("wrong");
+        }
+      }
+      this.ui.updateScore();
+      enteredKeys.length = 0;
+      return true;
+    }
+  };
+}
+
+// game statistics - the numbers
+export class Statistics {
+  constructor() {
+    this.correctCount = 0;
+    this.wrongCount = 0;
+  }
+
+  incrementScore = (count) => {
+    if (count === "correct") {
+      this.correctCount++;
+    } else if (count === "wrong") {
+      this.wrongCount++;
+    }
+    this.saveScore();
+  };
+
+  statsFound = () => {
+    if (localStorage.getItem("fastfingers-stats") === null) return false;
+    return true;
+  };
+
+  getStats = () => {
+    if (!this.statsFound()) {
+      let stats = { score: [this.correctCount, this.wrongCount] };
+      localStorage.setItem("fastfingers-stats", JSON.stringify(stats));
+      return JSON.parse(localStorage.getItem("fastfingers-stats"));
+    } else {
+      let stats = JSON.parse(localStorage.getItem("fastfingers-stats"));
+      return stats;
+    }
+  };
+
+  saveScore = () => {
+    let stats = this.getStats();
+    stats.score = [this.correctCount, this.wrongCount];
+    localStorage.setItem("fastfingers-stats", JSON.stringify(stats));
+  };
+}
--- js/index.js
+++ js/index.js
@@ -1,32 +1,39 @@
 import { UI } from "./app.js";
 import { Gameplay } from "./app.js";
+import { Statistics } from "./app.js";
 
 class App {
   constructor() {
     this.ui = new UI();
     this.gameplay = new Gameplay();
-    this.sound = document.querySelector("audio");
+    this.stats = new Statistics();
     this.enteredKeys = [];
   }
 
   events = () => {
-    this.ui.generateText();
     window.addEventListener("keypress", (e) => {
       // play key sound
-      this.sound.play();
+      this.ui.playKeySound();
       // update entered keys to compare with text
       this.enteredKeys.push(e.key);
       let textSpans = text.querySelectorAll("span");
+
       //  handle inputs - when sentence completes
       //  generate new text
-      const gameplay = new Gameplay();
-      let done = gameplay.handleInputLogic(this.enteredKeys, textSpans);
+      let done = this.gameplay.handleInputLogic(this.enteredKeys, textSpans);
       if (done) this.ui.generateText();
     });
   };
 }
 
+// start
 window.addEventListener("load", () => {
+  // instantiate App
   const app = new App();
   app.events();
+
+  //   instatiate UI
+  const ui = new UI();
+  ui.generateText();
+  ui.updateScore();
 });
